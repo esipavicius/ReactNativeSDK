fa821e50d0129bea3a45303d243bd0313891ee01
Ernestas Sipavičius <esipavicius@hotmail.com>
2024-12-11 18:40:44 +0200

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,28 +1,28 @@
 import { NativeModules, Platform } from 'react-native';
 
-const LINKING_ERROR =
-  `The package 'idenfy-react-native' doesn't seem to be linked. Make sure: \n\n` +
-  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
-  '- You rebuilt the app after installing the package\n' +
-  '- You are not using Expo Go\n';
+const getIdenfyReactNativeModule = () => {
+    const LINKING_ERROR =
+        `The package 'idenfy-react-native' doesn't seem to be linked. Make sure: \n\n` +
+        Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
+        '- You rebuilt the app after installing the package\n' +
+        '- You are not using Expo Go\n';
 
-const IdenfyReactNative = NativeModules.IdenfyReactNative
-  ? NativeModules.IdenfyReactNative
-  : new Proxy(
-      {},
-      {
-        get() {
-          throw new Error(LINKING_ERROR);
-        },
-      }
-    );
+    return NativeModules.IdenfyReactNative
+        ? NativeModules.IdenfyReactNative
+        : new Proxy(
+            {},
+            {
+                get() {
+                    throw new Error(LINKING_ERROR);
+                },
+            }
+        );
+}
 
 export function start(config: any): Promise<any> {
-  return IdenfyReactNative.start(config);
+  return getIdenfyReactNativeModule().start(config);
 }
 
 export function startFaceReAuth(config: any): Promise<any> {
-  return IdenfyReactNative.startFaceReAuth(config);
+  return getIdenfyReactNativeModule().startFaceReAuth(config);
 }
-
-
